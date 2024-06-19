dbf7e2407cab40a2d8fdc60d0c888dbe0e4b2312
nisafrnaa <53306051+NandaRiansya@users.noreply.github.com>
2024-06-18 22:15:27 -0700

    fix text account

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -2,7 +2,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Book Chill Perpus') }}
+            {{ __('AddBook Chill Perpus') }}
         </h2>
     </x-slot>
     <div class="container mx-auto my-10">
